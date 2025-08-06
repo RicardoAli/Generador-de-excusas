662254c6aa5f80585650663f1251b20f17564380
RicardoAli <61981276+RicardoAli@users.noreply.github.com>
2025-08-06 19:38:22 +0000

    First commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercise'];
+
+function generateExcuse() {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
+
+window.onload = () => {
+    document.getElementById("excuse").innerHTML = generateExcuse();
+};
